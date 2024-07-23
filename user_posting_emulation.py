b3fd71d088f0e6c8f63a918a09c68a22f98bbb84
54F-A <sarfaraj-ahmed@outlook.com>
2024-07-23 13:34:57 +0100

    Added docstrings to file.

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -13,8 +13,23 @@ random.seed(100)
 
 
 class AWSDBConnector:
+    """A class to handle AWS database connections.
+
+    Attributes:
+        config (dict): Configuration parameters loaded from the credentials file.
+        HOST (str): Database host address.
+        USER (str): Database username.
+        PASSWORD (str): Database password.
+        DATABASE (str): Database name.
+        PORT (int): Database port.
+    """
 
     def __init__(self, creds_file='db_creds.yaml'):
+        """Initialises the AWSDBConnector instance.
+
+        Args:
+            creds_file (str): The path to the database credentials YAML file.
+        """
         self.config = self.load_config(creds_file)
         self.HOST = self.config['HOST']
         self.USER = self.config['USER']
@@ -23,11 +38,23 @@ class AWSDBConnector:
         self.PORT = self.config['PORT']
     
     def load_config(self, creds_file):
+        """Loads database configuration from a YAML file.
+
+        Args:
+            creds_file (str): The path to the database credentials YAML file.
+        Returns:
+            dict: The configuration parameters.
+        """
         with open(creds_file, 'r') as file:
             config = yaml.safe_load(file)
         return config
         
     def create_db_connector(self):
+        """Creates a SQLAlchemy engine for connecting to the database.
+
+        Returns:
+            sqlalchemy.engine.Engine: The SQLAlchemy engine for the database connection.
+        """
         engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
         return engine
 
@@ -36,6 +63,8 @@ new_connector = AWSDBConnector()
 
 
 def run_infinite_post_data_loop():
+    """Runs infinite loop that selects random rows from multiple database tables and prints them.
+    """
     while True:
         sleep(random.randrange(0, 2))
         random_row = random.randint(0, 11000)
