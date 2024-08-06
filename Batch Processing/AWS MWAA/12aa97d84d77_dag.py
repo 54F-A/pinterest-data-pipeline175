346d6f28b3cdfc1424988c451739699d21d4eb29
54F-A <sarfaraj-ahmed@outlook.com>
2024-08-06 19:16:40 +0100

    Added DAG.

---
+++ Batch Processing/AWS MWAA/12aa97d84d77_dag.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+notebook_task = {
+    'notebook_path': '/Users/sarfaraj_ahmed@outlook.com/Clean',
+}
+
+
+notebook_params = {
+    "Variable":5
+}
+
+
+default_args = {
+    'owner': '12aa97d84d77',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG(
+    '12aa97d84d77_dag',
+    start_date=datetime(2024, 8, 6, 0, 0, 0),
+    schedule_interval='0 0 * * *',
+    catchup=False,
+    default_args=default_args
+) as dag:
+
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
